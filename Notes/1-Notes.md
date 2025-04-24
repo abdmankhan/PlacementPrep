e4b127d1b24ac300781a8e2cf78f36cfa5539ae9
Abdul Mannan Khan <abdmankhan@gmail.com>
2025-04-24 22:40:44 +0530

    5 - firebase auth & firestore working , login signup working

---
+++ Notes/1-Notes.md
@@ -0,0 +1,2 @@
+**1** Permission_Denied : Missing or insufficient permission when trying to add user to the firestore ???
+=> Solution , had to remove double inverted commas from the project id & client email from firebase .env.local
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,13 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "@/components/FormField";
 import { useRouter } from "next/navigation";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signUp, signIn } from "@/lib/actions/auth.action";
+
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -38,12 +45,48 @@ const AuthForm = ({ type } : {type : FormType}) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       if (type === "sign-up") {
+
+        const { name, email, password } = values;
+
+        // console.log({name, email, password});
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+        const result = await signUp({
+          uid : userCredential.user.uid,
+          name : name!,
+          email,
+          password
+        })
+
+        // console.log(`onSubmit function`, {name, email, password});
+        if(!result?.success) {
+          toast.error(result.message);
+          return;
+        }
+
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
+
+        const { email, password } = values;
+        console.log({email, password});
+        const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+
+        const idToken = await userCredentials.user.getIdToken();
+
+        if(!idToken) {
+          toast.error('Sign in failed.');
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken
+        })
+
         toast.success("Logged in successfully.");
         router.push("/");
       }
